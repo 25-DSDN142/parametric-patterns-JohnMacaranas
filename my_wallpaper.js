a8b69712f327fa3709771c1ddc76502b7bdbca49
John Macaranas <johnmacaranas07@gmail.com>
2025-08-03 01:06:44 +1200

    Update my_wallpaper.js
    water pattern

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,24 +1,24 @@
 //your parameter variables go here!
-let topLeft = 'Lily' //define top elements (Cat, Capybara, Monkey, Lily, Boat)
-let topRight = 'Capybara'
-let botLeft = 'Cat'
-let botRight = 'Lily'
-let iconSize = 50; //icon size 25-75
+let topLeft = '' //define top elements (Cat, Capybara, Monkey, Lily, Boat)
+let topRight = ''
+let botLeft = ''
+let botRight = ''
+let iconSize = 25; //icon size 25-75
 let iconTilt = 20; //tilt in degrees
 let lilyRotation = 190;
+let waterLayer = 3; // 1 - 3
+// let waterDepth = 10 //distance between waterLayer
 
 
 
 let pupilColour = 50;
 let catColour = 50;
 let centre = 100;
-let quart = 200/4
-
-
+let quart = 200/4;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -26,18 +26,51 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 100;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
 
 function wallpaper_background() {
-  background(25, 59, 92); //light honeydew green colour
+  background(68, 134, 158); //light honeydew green colour
 }
 
 function my_symbol(){ // do not rename this function. Treat this similarly to a Draw function
 
-  push();
-  sketch1(); //background pattern
-  pop();
+
+
+
+if(waterLayer == '3'){
+push();
+translate(75, 15);
+Bubbles3();
+pop();
+
+push();
+translate(25, 10);
+Bubbles2();
+
+pop();
+push();
+Bubbles1();
+pop();
+
+} else if(waterLayer == '2'){
+push();
+translate(25, 10);
+Bubbles2();
+
+pop();
+push();
+Bubbles1();
+pop();
+
+} else {
+
+push();
+Bubbles1();
+pop();
+}
+
+  
   
   push();
   translate(quart, quart)
@@ -121,6 +154,420 @@ function my_symbol(){ // do not rename this function. Treat this similarly to a 
 
 }
   
+function Bubbles1(){
+let bub;
+
+bub = createGraphics(200, 200);
+
+translate(100,100);
+
+bub.pixelDensity(4);
+
+bub.push();
+bub.strokeWeight(0);
+bub.rect(0, 0, 200, 200);
+bub.pop();
+
+bub.erase();
+bub.beginShape();
+bub.vertex(-46, -5);
+bub.bezierVertex(-41, -38, -14, -43, 4, -40);
+bub.bezierVertex(22, -37, 63, -12, 66, 20);
+bub.bezierVertex(70, 53, 30, 88, 0, 81);
+bub.bezierVertex(-30, 74, -30, 66, -29, 61);
+bub.bezierVertex(-28, 57, -12, 42, -7, 29);
+bub.bezierVertex(-2, 16, 1, 10, -9, 2);
+bub.bezierVertex(-28, -4, -38, 10, -46, -5);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(69, 75);
+bub.vertex(69, 75);
+bub.bezierVertex(79, 62, 105, 43, 126, 40);
+bub.bezierVertex(79, 62, 105, 43, 126, 40);
+bub.bezierVertex(146, 37, 154, 42, 163, 56);
+bub.bezierVertex(146, 37, 154, 42, 163, 56);
+bub.bezierVertex(167, 68, 160, 74, 166, 80);
+bub.bezierVertex(167, 68, 160, 74, 166, 80);
+bub.bezierVertex(173, 83, 175, 82, 180, 88);
+bub.bezierVertex(173, 83, 175, 82, 180, 88);
+bub.bezierVertex(185, 93, 192, 128, 179, 146);
+bub.bezierVertex(185, 93, 192, 128, 179, 146);
+bub.bezierVertex(163, 162, 158, 140, 150, 181);
+bub.bezierVertex(163, 162, 158, 140, 150, 181);
+bub.bezierVertex(135, 215, 103, 187, 113, 163);
+bub.bezierVertex(135, 215, 103, 187, 113, 163);
+bub.bezierVertex(118, 152, 129, 128, 120, 106);
+bub.bezierVertex(118, 152, 129, 128, 120, 106);
+bub.bezierVertex(111, 84, 96, 80, 87, 82);
+bub.bezierVertex(111, 84, 96, 80, 87, 82);
+bub.bezierVertex(77, 84, 59, 88, 69, 75);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(66, 59);
+bub.bezierVertex(78, 32, 75, 20, 74, 13);
+bub.bezierVertex(74, 7, 73, -3, 91, -8);
+bub.bezierVertex(109, -13, 120, -1, 122, 3);
+bub.bezierVertex(125, 6, 115, 9, 115, 15);
+bub.bezierVertex(115, 21, 114, 23, 118, 26);
+bub.bezierVertex(121, 29, 122, 31, 109, 35);
+bub.bezierVertex(96, 40, 85, 49, 80, 55);
+bub.bezierVertex(75, 61, 55, 84, 66, 59);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(122, 17);
+bub.bezierVertex(126, 4, 135, 5, 141, 5);
+bub.bezierVertex(147, 5, 150, 5, 154, 8);
+bub.bezierVertex(157, 11, 173, 9, 181, 11);
+bub.bezierVertex(194, 16, 191, 19, 185, 26);
+bub.bezierVertex(179, 33, 174, 39, 172, 43);
+bub.bezierVertex(170, 47, 165, 44, 163, 38);
+bub.bezierVertex(161, 31, 148, 28, 142, 28);
+bub.bezierVertex(136, 28, 118, 31, 122, 17);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(4, 92);
+bub.bezierVertex(13, 90, 29, 88, 36, 84);
+bub.bezierVertex(43, 80, 47, 81, 48, 85);
+bub.bezierVertex(49, 89, 51, 94, 73, 93);
+bub.bezierVertex(95, 92, 120, 101, 119, 131);
+bub.bezierVertex(118, 149, 99, 165, 112, 186);
+bub.bezierVertex(91, 180, 74, 192, 72, 206);
+bub.bezierVertex(52, 163, 25, 151, 6, 152);
+bub.bezierVertex(-6, 152, -6, 131, -7, 122);
+bub.bezierVertex(-6, 110, -5, 95, 4, 92);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(-47, 193);
+bub.bezierVertex(-41, 161, -14, 156, 4, 159);
+bub.bezierVertex(22, 162, 63, 186, 66, 219);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(153, -6);
+bub.bezierVertex(162, 10, 172, -5, 191, 2);
+bub.bezierVertex(201, 10, 199, 17, 193, 29);
+bub.bezierVertex(188, 42, 172, 56, 171, 61);
+bub.bezierVertex(170, 65, 169, 74, 200, 81);
+bub.vertex(200, 0);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(205, 92);
+bub.bezierVertex(195, 95, 194, 111, 193, 122);
+bub.bezierVertex(194, 131, 194, 151, 206, 152);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(204, 160);
+bub.bezierVertex(186, 157, 160, 162, 153, 194);
+bub.bezierVertex(162, 210, 172, 195, 191, 202);
+bub.vertex(200, 200);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(73, 214);
+bub.bezierVertex(73, 208, 72, 198, 91, 193);
+bub.bezierVertex(109, 188, 119, 201, 122, 204);
+bub.endShape(CLOSE);
+
+
+bub.ellipse(58, 80, 11, 13);
+bub.ellipse(50, 73, 4, 4);
+bub.ellipse(191, 90, 10, 10);
+bub.ellipse(185, 85, 5, 5);
+bub.ellipse(-1, 89, 3, 3);
+bub.ellipse(148, 0, 7, 7);
+bub.ellipse(148, 199, 7, 7);
+bub.ellipse(199, 89, 3, 3);
+bub.ellipse(190, 150, 10, 7);
+bub.ellipse(189, 143, 5, 4);
+
+noErase();
+
+image(bub, 0, 0);
+}
+
+function Bubbles2(){
+let bub;
+
+bub = createGraphics(200, 200);
+
+translate(100,100);
+
+bub.pixelDensity(4);
+
+bub.push();
+bub.fill(95, 166, 180);
+bub.strokeWeight(0);
+bub.rect(0, 0, 200, 200);
+bub.pop();
+
+bub.erase();
+bub.beginShape();
+bub.vertex(-46, -5);
+bub.bezierVertex(-41, -38, -14, -43, 4, -40);
+bub.bezierVertex(22, -37, 63, -12, 66, 20);
+bub.bezierVertex(70, 53, 30, 88, 0, 81);
+bub.bezierVertex(-30, 74, -30, 66, -29, 61);
+bub.bezierVertex(-28, 57, -12, 42, -7, 29);
+bub.bezierVertex(-2, 16, 1, 10, -9, 2);
+bub.bezierVertex(-28, -4, -38, 10, -46, -5);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(69, 75);
+bub.vertex(69, 75);
+bub.bezierVertex(79, 62, 105, 43, 126, 40);
+bub.bezierVertex(79, 62, 105, 43, 126, 40);
+bub.bezierVertex(146, 37, 154, 42, 163, 56);
+bub.bezierVertex(146, 37, 154, 42, 163, 56);
+bub.bezierVertex(167, 68, 160, 74, 166, 80);
+bub.bezierVertex(167, 68, 160, 74, 166, 80);
+bub.bezierVertex(173, 83, 175, 82, 180, 88);
+bub.bezierVertex(173, 83, 175, 82, 180, 88);
+bub.bezierVertex(185, 93, 192, 128, 179, 146);
+bub.bezierVertex(185, 93, 192, 128, 179, 146);
+bub.bezierVertex(163, 162, 158, 140, 150, 181);
+bub.bezierVertex(163, 162, 158, 140, 150, 181);
+bub.bezierVertex(135, 215, 103, 187, 113, 163);
+bub.bezierVertex(135, 215, 103, 187, 113, 163);
+bub.bezierVertex(118, 152, 129, 128, 120, 106);
+bub.bezierVertex(118, 152, 129, 128, 120, 106);
+bub.bezierVertex(111, 84, 96, 80, 87, 82);
+bub.bezierVertex(111, 84, 96, 80, 87, 82);
+bub.bezierVertex(77, 84, 59, 88, 69, 75);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(66, 59);
+bub.bezierVertex(78, 32, 75, 20, 74, 13);
+bub.bezierVertex(74, 7, 73, -3, 91, -8);
+bub.bezierVertex(109, -13, 120, -1, 122, 3);
+bub.bezierVertex(125, 6, 115, 9, 115, 15);
+bub.bezierVertex(115, 21, 114, 23, 118, 26);
+bub.bezierVertex(121, 29, 122, 31, 109, 35);
+bub.bezierVertex(96, 40, 85, 49, 80, 55);
+bub.bezierVertex(75, 61, 55, 84, 66, 59);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(122, 17);
+bub.bezierVertex(126, 4, 135, 5, 141, 5);
+bub.bezierVertex(147, 5, 150, 5, 154, 8);
+bub.bezierVertex(157, 11, 173, 9, 181, 11);
+bub.bezierVertex(194, 16, 191, 19, 185, 26);
+bub.bezierVertex(179, 33, 174, 39, 172, 43);
+bub.bezierVertex(170, 47, 165, 44, 163, 38);
+bub.bezierVertex(161, 31, 148, 28, 142, 28);
+bub.bezierVertex(136, 28, 118, 31, 122, 17);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(4, 92);
+bub.bezierVertex(13, 90, 29, 88, 36, 84);
+bub.bezierVertex(43, 80, 47, 81, 48, 85);
+bub.bezierVertex(49, 89, 51, 94, 73, 93);
+bub.bezierVertex(95, 92, 120, 101, 119, 131);
+bub.bezierVertex(118, 149, 99, 165, 112, 186);
+bub.bezierVertex(91, 180, 74, 192, 72, 206);
+bub.bezierVertex(52, 163, 25, 151, 6, 152);
+bub.bezierVertex(-6, 152, -6, 131, -7, 122);
+bub.bezierVertex(-6, 110, -5, 95, 4, 92);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(-47, 193);
+bub.bezierVertex(-41, 161, -14, 156, 4, 159);
+bub.bezierVertex(22, 162, 63, 186, 66, 219);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(153, -6);
+bub.bezierVertex(162, 10, 172, -5, 191, 2);
+bub.bezierVertex(201, 10, 199, 17, 193, 29);
+bub.bezierVertex(188, 42, 172, 56, 171, 61);
+bub.bezierVertex(170, 65, 169, 74, 200, 81);
+bub.vertex(200, 0);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(205, 92);
+bub.bezierVertex(195, 95, 194, 111, 193, 122);
+bub.bezierVertex(194, 131, 194, 151, 206, 152);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(204, 160);
+bub.bezierVertex(186, 157, 160, 162, 153, 194);
+bub.bezierVertex(162, 210, 172, 195, 191, 202);
+bub.vertex(200, 200);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(73, 214);
+bub.bezierVertex(73, 208, 72, 198, 91, 193);
+bub.bezierVertex(109, 188, 119, 201, 122, 204);
+bub.endShape(CLOSE);
+
+bub.ellipse(58, 80, 11, 13);
+bub.ellipse(50, 73, 4, 4);
+bub.ellipse(191, 90, 10, 10);
+bub.ellipse(185, 85, 5, 5);
+bub.ellipse(-1, 89, 3, 3);
+bub.ellipse(148, 0, 7, 7);
+bub.ellipse(148, 199, 7, 7);
+bub.ellipse(199, 89, 3, 3);
+bub.ellipse(190, 150, 10, 7);
+bub.ellipse(189, 143, 5, 4);
+
+noErase();
+
+image(bub, 0, 0);
+}
+
+function Bubbles3(){
+let bub;
+
+bub = createGraphics(200, 200);
+
+translate(100,100);
+
+bub.pixelDensity(4);
+
+bub.push();
+bub.fill(80, 150, 164);
+bub.strokeWeight(0);
+bub.rect(0, 0, 200, 200);
+bub.pop();
+
+bub.erase();
+bub.beginShape();
+bub.vertex(-46, -5);
+bub.bezierVertex(-41, -38, -14, -43, 4, -40);
+bub.bezierVertex(22, -37, 63, -12, 66, 20);
+bub.bezierVertex(70, 53, 30, 88, 0, 81);
+bub.bezierVertex(-30, 74, -30, 66, -29, 61);
+bub.bezierVertex(-28, 57, -12, 42, -7, 29);
+bub.bezierVertex(-2, 16, 1, 10, -9, 2);
+bub.bezierVertex(-28, -4, -38, 10, -46, -5);
+bub.endShape(CLOSE);
+
+beginShape();
+bub.beginShape();
+bub.vertex(69, 75);
+bub.vertex(69, 75);
+bub.bezierVertex(79, 62, 105, 43, 126, 40);
+bub.bezierVertex(79, 62, 105, 43, 126, 40);
+bub.bezierVertex(146, 37, 154, 42, 163, 56);
+bub.bezierVertex(146, 37, 154, 42, 163, 56);
+bub.bezierVertex(167, 68, 160, 74, 166, 80);
+bub.bezierVertex(167, 68, 160, 74, 166, 80);
+bub.bezierVertex(173, 83, 175, 82, 180, 88);
+bub.bezierVertex(173, 83, 175, 82, 180, 88);
+bub.bezierVertex(185, 93, 192, 128, 179, 146);
+bub.bezierVertex(185, 93, 192, 128, 179, 146);
+bub.bezierVertex(163, 162, 158, 140, 150, 181);
+bub.bezierVertex(163, 162, 158, 140, 150, 181);
+bub.bezierVertex(135, 215, 103, 187, 113, 163);
+bub.bezierVertex(135, 215, 103, 187, 113, 163);
+bub.bezierVertex(118, 152, 129, 128, 120, 106);
+bub.bezierVertex(118, 152, 129, 128, 120, 106);
+bub.bezierVertex(111, 84, 96, 80, 87, 82);
+bub.bezierVertex(111, 84, 96, 80, 87, 82);
+bub.bezierVertex(77, 84, 59, 88, 69, 75);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(66, 59);
+bub.bezierVertex(78, 32, 75, 20, 74, 13);
+bub.bezierVertex(74, 7, 73, -3, 91, -8);
+bub.bezierVertex(109, -13, 120, -1, 122, 3);
+bub.bezierVertex(125, 6, 115, 9, 115, 15);
+bub.bezierVertex(115, 21, 114, 23, 118, 26);
+bub.bezierVertex(121, 29, 122, 31, 109, 35);
+bub.bezierVertex(96, 40, 85, 49, 80, 55);
+bub.bezierVertex(75, 61, 55, 84, 66, 59);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(122, 17);
+bub.bezierVertex(126, 4, 135, 5, 141, 5);
+bub.bezierVertex(147, 5, 150, 5, 154, 8);
+bub.bezierVertex(157, 11, 173, 9, 181, 11);
+bub.bezierVertex(194, 16, 191, 19, 185, 26);
+bub.bezierVertex(179, 33, 174, 39, 172, 43);
+bub.bezierVertex(170, 47, 165, 44, 163, 38);
+bub.bezierVertex(161, 31, 148, 28, 142, 28);
+bub.bezierVertex(136, 28, 118, 31, 122, 17);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(4, 92);
+bub.bezierVertex(13, 90, 29, 88, 36, 84);
+bub.bezierVertex(43, 80, 47, 81, 48, 85);
+bub.bezierVertex(49, 89, 51, 94, 73, 93);
+bub.bezierVertex(95, 92, 120, 101, 119, 131);
+bub.bezierVertex(118, 149, 99, 165, 112, 186);
+bub.bezierVertex(91, 180, 74, 192, 72, 206);
+bub.bezierVertex(52, 163, 25, 151, 6, 152);
+bub.bezierVertex(-6, 152, -6, 131, -7, 122);
+bub.bezierVertex(-6, 110, -5, 95, 4, 92);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(-47, 193);
+bub.bezierVertex(-41, 161, -14, 156, 4, 159);
+bub.bezierVertex(22, 162, 63, 186, 66, 219);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(153, -6);
+bub.bezierVertex(162, 10, 172, -5, 191, 2);
+bub.bezierVertex(201, 10, 199, 17, 193, 29);
+bub.bezierVertex(188, 42, 172, 56, 171, 61);
+bub.bezierVertex(170, 65, 169, 74, 200, 81);
+bub.vertex(200, 0);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(205, 92);
+bub.bezierVertex(195, 95, 194, 111, 193, 122);
+bub.bezierVertex(194, 131, 194, 151, 206, 152);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(204, 160);
+bub.bezierVertex(186, 157, 160, 162, 153, 194);
+bub.bezierVertex(162, 210, 172, 195, 191, 202);
+bub.vertex(200, 200);
+bub.endShape(CLOSE);
+
+bub.beginShape();
+bub.vertex(73, 214);
+bub.bezierVertex(73, 208, 72, 198, 91, 193);
+bub.bezierVertex(109, 188, 119, 201, 122, 204);
+bub.endShape(CLOSE);
+
+bub.ellipse(58, 80, 11, 13);
+bub.ellipse(50, 73, 4, 4);
+bub.ellipse(191, 90, 10, 10);
+bub.ellipse(185, 85, 5, 5);
+bub.ellipse(-1, 89, 3, 3);
+bub.ellipse(148, 0, 7, 7);
+bub.ellipse(148, 199, 7, 7);
+bub.ellipse(199, 89, 3, 3);
+bub.ellipse(190, 150, 10, 7);
+bub.ellipse(189, 143, 5, 4);
+
+noErase();
+
+image(bub, 0, 0);
+}
 
 function Cat(){
 scale(0.85);
@@ -215,6 +662,7 @@ function Capybara(){
 
 function Lily(){
   rotate(lilyRotation);
+  translate(25, 0);
   lily = createGraphics(200, 200);
   
   lily.pixelDensity(5);
@@ -237,37 +685,6 @@ function Lily(){
  
     image(lily, 0, 0);
 
-}
-
-function bubbles(){
-
-  fill(250);
-  strokeWeight(0);
-  
-  beginShape();
-  vertex(10, 50);
-  bezierVertex(10, 10, 20, 10, 50, 10);
-  bezierVertex(80, 10, 60, 40, 50, 50);
-  bezierVertex(30, 70, 10, 60, 10, 50);
-  bezierVertex()
-  endShape(CLOSE);
-  
-  beginShape();
-  vertex(80, 20);
-  bezierVertex(90, 10, 110, 10, 120, 40);
-  bezierVertex(120, 60, 160, 60, 170, 80);
-  bezierVertex(190, 130, 40, 90, 70, 40);
-  endShape(CLOSE);
-  
-  beginShape();
-  vertex(40, 80);
-  bezierVertex(50, 60, 60, 70, 60, 70);
-  bezierVertex(70, 80, 70, 90, 70, 90);
-  bezierVertex(70, 120, 30, 90, 40, 80);
-  endShape(CLOSE);
-  
-  ellipse(60, 60, 5, 5);
-  ellipse(30, 75, 10, 10);
 }
 
 function sketch1(){
